008c8cb3c600962ae8250beede57aafc2316b9ba
MANISH <manishpatel56709@gmail.com>
2025-08-10 00:46:54 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (9.94%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = -1;
+        int j = -1; // set j = -1 (-1 means I haven’t found any zero yet.) This variable will track the position 
+                    //of the first zero found in the array.
         int n = nums.size();
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){  // whenver we find any zero store its index in  "j"
             if(nums[i] == 0){
                 j = i;
                 break;
@@ -14,7 +15,7 @@ public:
             return;
          
 
-         for(int i = j + 1; i <n; i++) {
+         for(int i = j + 1; i < n; i++) {
             if(nums[i] != 0) {
                 swap(nums[i], nums[j]);
                 j++;
