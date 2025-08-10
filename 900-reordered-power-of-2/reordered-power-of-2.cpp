3e4a1f3a70a4ac100c078b3a134cae83d6754ac3
MANISH <manishpatel56709@gmail.com>
2025-08-11 00:15:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (34.22%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -8,8 +8,11 @@ public:
     bool reorderedPowerOf2(int n) {
         string s = getSortedStr(n);
 
-        for(int p = 0; p<=29; p++){
-            if(s== getSortedStr(1 << p)){
+        //check all  2 powers and see if it match with 
+        // We need only 2^0 to 2^29 becouse in ques it is given the range of n is 10^9 and 2^30 is out of the range
+
+        for(int p = 0; p<=29; p++){ //p = power
+            if(s == getSortedStr(1 << p)){ //leftshift 1 to get power
                 return true;
             }
         }
