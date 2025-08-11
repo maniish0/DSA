aaf4e826d666b8a154ba9711dac41f24fb4cae8b
MANISH <manishpatel56709@gmail.com>
2025-08-11 16:40:51 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (29.95%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -6,7 +6,7 @@ public:
       for(int i = 0; i< nums.size(); i++){
         if(nums[i] == 1){
            cnt++;
-           maxi =max(maxi,cnt);
+           maxi = max(maxi,cnt);
         }
         else {
             cnt = 0;
@@ -18,3 +18,6 @@ public:
 
     }
 };
+
+
+// we star fron the first element to the end of the arr and checks repeation of "1" and store in  "cnt" and then return the max (store in maxi) of "cnt"
