330b190e83947bc64bbd961502d05f0b1b484011
MANISH <manishpatel56709@gmail.com>
2025-08-20 15:06:37 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (25.81%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,10 +4,12 @@ public:
         int mini = prices[0];
         int maxprofit = 0;
         int n = prices.size();
+
+    
         for (int i= 0; i<n; i++){
-            int cost = prices[i]- mini;
-            maxprofit =max(maxprofit , cost);
-            mini = min(mini,prices[i]);
+            int cost = prices[i]- mini; //profit if we sold at the same day
+            maxprofit =max(maxprofit , cost);  //update best profit
+            mini = min(mini,prices[i]);  // update lowest price
         }
         return maxprofit;
     }
