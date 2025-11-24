f02a0b8ca442b77e90c7a388fe699735f74309b4
MANISH <manishpatel56709@gmail.com>
2025-11-24 22:44:33 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (85.02%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,19 +1,51 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> res(nums.size(), 0);
-        int left = 0;
-        int right = nums.size() - 1;
+    
+    //APPROACH 1
 
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            if (abs(nums[left]) > abs(nums[right])) {
-                res[i] = nums[left] * nums[left];
-                left++;
-            } else {
-                res[i] = nums[right] * nums[right];
-                right--;
-            }
-        }
-        return res;        
+    //step 1 (sq the elements)
+
+    for(int i = 0; i < nums.size(); i++){
+        nums[i] = (nums[i] * nums[i]);
+    }
+
+    //step 2: (sort the arr)
+    sort(begin(nums), end(nums));
+
+    return nums;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     vector<int> res(nums.size(), 0);
+    //     int left = 0;
+    //     int right = nums.size() - 1;
+
+    //     for (int i = nums.size() - 1; i >= 0; i--) {
+    //         if (abs(nums[left]) > abs(nums[right])) {
+    //             res[i] = nums[left] * nums[left];
+    //             left++;
+    //         } else {
+    //             res[i] = nums[right] * nums[right];
+    //             right--;
+    //         }
+    //     }
+    //     return res;        
     }
 };
