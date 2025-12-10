554c8016bdd1baba74411bd830bf2101069f697d
MANISH <manishpatel56709@gmail.com>
2025-12-10 18:38:25 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (99.61%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,8 +3,9 @@ public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
         int Sum=0;
-        for(int i:nums){
-            Sum+=i;
+        for(int i=0; i<n; i++){
+            int value = nums[i];
+            Sum+=value;
         }
        return n*(n+1)/2-Sum; //formula of sum of first natural numbers
     }
