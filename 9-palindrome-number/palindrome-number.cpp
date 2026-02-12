bfe4aac45033287adce1e0f149689697b6f15438
MANISH <manishpatel56709@gmail.com>
2026-02-13 00:27:53 +0530

    Time: 2 ms (51.02%) | Memory: 8.5 MB (65.75%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        if (x < 0) return false;
+
+        int rev = 0;
+        int original = x;
+
+        while (x > 0) {
+            int digit = x % 10;
+
+            if (rev > (INT_MAX - digit) / 10) return false;
+
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+
+        return rev == original;
+    }
+};
