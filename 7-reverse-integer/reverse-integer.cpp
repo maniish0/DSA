f6b55a446168e9592afd288298728e76072490f7
MANISH <manishpatel56709@gmail.com>
2026-02-13 17:20:52 +0530

    Time: 5 ms (19.94%) | Memory: 8.5 MB (82.39%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -4,11 +4,13 @@ public:
         long newX = 0, remDigits = 0;
         while(x != 0){
             remDigits = x % 10;
+
+          // Check for overflow/underflow before updating ans
+            if ((newX > INT_MAX / 10) || (newX < INT_MIN / 10)) {
+                return 0; // Return 0 if reversing x would cause overflow/underflow
+            }
             newX = newX * 10 + remDigits;
             x /= 10;
-        }
-        if(newX > INT_MAX || newX < INT_MIN){
-            return 0;
         }
 
         return newX;
