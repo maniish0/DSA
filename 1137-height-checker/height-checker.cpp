0c1ec1a8b062f5346e037d5b8923601ea0ffd8bd
MANISH <manishpatel56709@gmail.com>
2026-02-20 01:18:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (89.23%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> heightToFreq(101, 0);
+
+
+
+        vector<int> heightToFreq(101, 0); 
         
-        // Count frequency of each height
+
+        /* Count frequency of each height
+        here we count the freq of each elements present in the array 
+        for example : {1,1,4,2,1,3} 
+        the freq of each ele are f[1] = 3, f[2] = 1 , f[3] = 1, f[4] = 1;
+        */
         for (int height : heights) {
             heightToFreq[height]++;
         }
         
-        int result = 0;
+        int NumOFIncrrt = 0; // here we count the number of incorrect position of the students in a array.
+
+
         int curHeight = 0;
         
         // Compare with expected sorted order
         for (int i = 0; i < heights.size(); i++) {
+          
             while (heightToFreq[curHeight] == 0) {
                 curHeight++;
             }
             
             if (curHeight != heights[i]) {
-                result++;
+                NumOFIncrrt++;
             }
             
             heightToFreq[curHeight]--;
         }
         
-        return result;
+        return NumOFIncrrt;
     }
 };
