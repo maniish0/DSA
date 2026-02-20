1eaff9dee49dc547ba9b533c2d36dd265bb99052
MANISH <manishpatel56709@gmail.com>
2026-02-20 10:48:23 +0530

    Time: 8 ms (59.99%) | Memory: 44.8 MB (47.61%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,25 +1,51 @@
+// class Solution {
+// public:
+//     // Function to find the maximum number of content students
+//     int findContentChildren(vector<int>& student, vector<int>& cookie) {
+//         // Sort both arrays to apply the greedy strategy
+//         sort(student.begin(), student.end());
+//         sort(cookie.begin(), cookie.end());
+
+//         int studentIndex = 0; 
+//         int cookieIndex = 0;  
+
+//         // Try to assign cookies until any one list is fully processed
+//         while (studentIndex < student.size() && cookieIndex < cookie.size()) {
+//             // If the cookie satisfies the student's greed
+//             if (cookie[cookieIndex] >= student[studentIndex]) {
+//                 studentIndex++; 
+//             }
+//             // Move to next cookie in both cases
+//             cookieIndex++; 
+//         }
+
+//         // Number of students satisfied is equal to studentIndex
+//         return studentIndex;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
-    // Function to find the maximum number of content students
-    int findContentChildren(vector<int>& student, vector<int>& cookie) {
-        // Sort both arrays to apply the greedy strategy
-        sort(student.begin(), student.end());
-        sort(cookie.begin(), cookie.end());
-
-        int studentIndex = 0; 
-        int cookieIndex = 0;  
-
-        // Try to assign cookies until any one list is fully processed
-        while (studentIndex < student.size() && cookieIndex < cookie.size()) {
-            // If the cookie satisfies the student's greed
-            if (cookie[cookieIndex] >= student[studentIndex]) {
-                studentIndex++; 
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i=0;
+        int j=0;
+        while(i<g.size() and j<s.size()) {
+            if(g[i]<=s[j]) {
+                i++;
             }
-            // Move to next cookie in both cases
-            cookieIndex++; 
+            j++;
         }
-
-        // Number of students satisfied is equal to studentIndex
-        return studentIndex;
+        return i;
     }
 };
