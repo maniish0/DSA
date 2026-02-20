073b768fc1d237d633c62a8ea48847ff843c4812
MANISH <manishpatel56709@gmail.com>
2026-02-21 00:57:07 +0530

    Time: 0 ms (100.00%) | Memory: 312.1 MB (16.49%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
@@ -8,27 +8,61 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* deleteMiddle(ListNode* head) {
+//         ListNode* prevSlow  = NULL;
+//         ListNode* slow      = head;
+//         ListNode* fast      = head;
+        
+//         while(fast && fast->next) {
+//             prevSlow = slow;
+//             slow = slow->next;
+            
+//             fast = fast->next->next;
+//         }
+        
+//         if(prevSlow == NULL) {
+//             return NULL;
+//         }
+        
+//         prevSlow->next = slow->next; 
+//          //here we point the previous node (before the slow where the middle found) to the next Node to the middle of the LL. 
+//         // For Eg : 1,2,3,4   And in this ll the mid is "3" then we point the "2" -> "4" by USING " prevSlow->next = slow->next;"
+        
+//         delete(slow);
+        
+//         return head;
+//     }
+// };
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        ListNode* prevSlow  = NULL;
-        ListNode* slow      = head;
-        ListNode* fast      = head;
-        
-        while(fast && fast->next) {
+        ListNode* prevSlow = NULL;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next){
             prevSlow = slow;
-            slow     = slow->next;
-            
-            fast     = fast->next->next;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        
-        if(prevSlow == NULL) {
+        if(prevSlow == NULL){
             return NULL;
         }
-        
         prevSlow->next = slow->next;
-        delete(slow);
-        
+
+        delete (slow);
+
         return head;
+
+
+
+
+
+
+
+
+        
     }
 };
