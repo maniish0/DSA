8def98d5f1b86ef60a37390f2ada51f6af7efe53
MANISH <manishpatel56709@gmail.com>
2026-03-05 01:54:10 +0530

    Time: 3 ms (67.94%) | Memory: 14.9 MB (35.94%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,46 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> pairIdx;
 
-
-        map<int,int> mpp;
-        int n  = nums.size();
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < nums.size(); i++) {
             int num = nums[i];
-            int moreNeeded = target - num;
-            if(mpp.find(moreNeeded) != mpp.end()){
-                return{mpp[moreNeeded], i};
-        
+            if (pairIdx.find(target - num) != pairIdx.end()) {
+                return {i, pairIdx[target - num]};
             }
-
-            mpp[num] =i ;    //If no match is found, store the current number and its index in the map.This way, future iterations can check if this number is the missing pair for some later element.
-
-
+            pairIdx[num] = i;
         }
-        
-       
-       return {-1,1} ;
+
+        return {};        
     }
 };
-
-
-
-
-
-
-       // More better the previous one solution
-
-       //int left = a , right =n-1;
-    //    sort(book.begin() , book.end());
-    //    while(left <right){
-    //     int sum = book[left] + book[right];
-    //     if(sum == target) {
-    //         return "YES";
-    //     }
-
-    //     else if (sum < target) left++;
-    //     else right--;
-    //    }
-    // return "NO";
-
-    
