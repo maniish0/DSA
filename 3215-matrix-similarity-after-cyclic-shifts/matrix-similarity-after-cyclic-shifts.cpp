5d79688ddb248d7e39292820bc719083983e00c9
MANISH <manishpatel56709@gmail.com>
2026-04-06 00:22:20 +0530

    Time: 3 ms (24.63%) | Memory: 33.9 MB (5.09%) - LeetSync

---
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        vector<vector<int>> temp = mat;
+        int m = mat.size();
+        int n = mat[0].size();
+        k = k % n; // very import line after k rotation the outputs are repeating
+                  // itself again and again.
+
+            for (int i = 0; i < mat.size(); i++) {
+            if (i % 2 == 0) {
+                rotate(rbegin(mat[i]), rbegin(mat[i]) + k, rend(mat[i]));
+            } else {
+                rotate(begin(mat[i]), begin(mat[i]) + k, end(mat[i]));
+            }
+        }
+        return temp == mat;
+    }
+};
