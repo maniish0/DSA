da0641c4089bead376bc9555ac4d6e79c2896ddb
MANISH <manishpatel56709@gmail.com>
2026-04-14 22:23:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (67.27%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int end = s.length() -1;
+        int end = s.length()-1;
+       
         while(end >= 0 && s[end] == ' '){
             end--;
+
         }
-        int start = end;
+
+        int  start = end;
+
         while(start >= 0 && s[start] != ' '){
             start--;
         }
+
         return end - start;
+        
     }
 };
