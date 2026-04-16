912db11f3b81ad0dc30b024092ea2df956d78f7c
MANISH <manishpatel56709@gmail.com>
2026-04-16 17:54:31 +0530

    Time: 3 ms (3.03%) | Memory: 10.9 MB (41.91%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -1,4 +1,4 @@
-/*
+/**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
@@ -9,38 +9,26 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-
-
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        if(root == NULL)return preorder;
 
-    // preorder means "ROOT LEFT RIGHT"
-    /*
-    without using RECURTION we solvr this problem by using stack DS
-    stack(LIFO)     
-    
-    */
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
 
-    vector<int> preorder;
-    if(root == NULL) return preorder;
-
-    stack<TreeNode*> st;
-    st.push(root);
-    while(!st.empty()){
-        root = st.top();
-        st.pop();
-        preorder.push_back(root->val);
-        if(root->right != NULL){
-            st.push(root->right);
+            preorder.push_back(root -> val);
+            if(root -> right != NULL){
+                st.push(root ->right);
+            }
+             if(root -> left != NULL){
+                st.push(root ->left);
+            }
         }
-        if(root->left != NULL){
-            st.push(root->left);
-        }
-    }
-    return preorder;
-
-        
+        return preorder;
     }
 };
