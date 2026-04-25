0476518f0242a04db6e10801c2f2a2771711e234
MANISH <manishpatel56709@gmail.com>
2026-04-25 22:35:15 +0530

    Time: 6 ms (34.11%) | Memory: 12.8 MB (15.38%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        const vector<pair<int, string>> valueSymbols{
-            {1000,"M"}, {900,"CM"}, {500,"D"}, {400,"CD"},{100,"C"}
-            ,{90,"XC"}, {50,"L"}, {40, "XL"}, {10, "X"}, {9, "IX"},
-            {5,"V"}, {4,"IV"}, {1,"I"}
+        // Value-symbol pairs in descending order (include subtraction cases)
+        vector<pair<int, string>> values = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
+            {1, "I"}
         };
-        string result;
-
-        for(const auto& [value, symbols] : valueSymbols){
-            if (num == 0) break;
-
-            while(num >= value){
-                result += symbols;
+        
+        string result = "";
+        
+        // Greedy: subtract largest possible value each time
+        for (auto& [value, symbol] : values) {
+            while (num >= value) {
                 num -= value;
+                result += symbol;
             }
         }
-        return result;
         
+        return result;
     }
 };
