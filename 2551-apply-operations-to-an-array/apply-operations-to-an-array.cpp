f266aa220448dceef0742e9096dcaa5ef26ecab9
MANISH <manishpatel56709@gmail.com>
2026-05-03 00:12:21 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (88.25%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
+        
         int n = nums.size();
-        vector<int> result;
+        vector<int>result;
 
-        // Step 1: Merge equal neighbors
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
+        for(int i =0; i < n-1; i++){
+            if(nums[i] == nums[i+1]){
                 nums[i] *= 2;
-                nums[i + 1] = 0;
+                nums[i+1] = 0;
             }
         }
 
-        // Step 2: Collect non-zeros first
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             if (nums[i] != 0)
-                result.push_back(nums[i]);
+            result.push_back(nums[i]);
         }
 
-        // Step 3: Pad with zeros at the end
-        while (result.size() < n)
+        //move zeroes to the end
+        while(result.size() < n){
+            //sari places p zero put krdo
             result.push_back(0);
-
+        }
         return result;
     }
 };
