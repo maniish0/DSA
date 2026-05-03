eae99b4f2fab4260d7df6e30ed3113db927346bf
MANISH <manishpatel56709@gmail.com>
2026-05-03 22:56:57 +0530

    Time: 3 ms (6.54%) | Memory: 9.3 MB (7.46%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    // Check if 'goal' is a rotation of 's'
+    bool rotateString(string& s, string& goal) {
+        // Strings must be same length to be rotations of each other
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        // Try all possible rotations of 's'
+        for (int i = 0; i < s.length(); i++) {
+            string rotated = s.substr(i) + s.substr(0, i);  
+            if (rotated == goal) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
