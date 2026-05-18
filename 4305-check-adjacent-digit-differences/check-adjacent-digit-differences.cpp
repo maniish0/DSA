5d39570cde7b9d13502c60398518d5d182545bec
MANISH <manishpatel56709@gmail.com>
2026-05-18 08:41:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (75.00%) - LeetSync

---
+++ 4305-check-adjacent-digit-differences/check-adjacent-digit-differences.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        for(int i =0; i<s.size()-1; i++){
+            int a =s[i] -'0';
+            int b= s[i+1]-'0';
+
+            if(abs(a-b)> 2){
+                return false;
+            }
+        }
+        return true;
+    }
+};
