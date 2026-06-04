9b6a76128c6e10c3c70e9443e50951a3446f7c50
MANISH <manishpatel56709@gmail.com>
2026-06-04 07:28:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (40.88%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -3,22 +3,23 @@ public:
     int longestPalindrome(string s) {
        unordered_map<char,int> freq;
 
-       for(char c : s)
+       for(char c : s){
         freq[c]++;
+       }
 
         int length = 0; 
         bool hasOdd = false;
 
-         for (auto& [ch, count] : freq) {
-        length += (count / 2) * 2;  // take the largest even part
+        for (auto& [ch, count] : freq) {
+        length += (count / 2) * 2;  // take the largest even part..FOR EXAMPLE IF WE HAVE 5 THEN WE STORE THE LEN 4 IN THE lenght and drop 1
         if (count % 2 == 1)
             hasOdd = true;          // found an odd frequency char
-    }
+        }
 
-    if (hasOdd) length++;  // place one odd char in the middle, 
-    //the acctual meaning of the line is "IF hasOdd IS TRUE THEN INC THE LENGTH BY 1"
+        if (hasOdd) length++;  // place one odd char in the middle, 
+         //the acctual meaning of the line is "IF hasOdd IS TRUE THEN INC THE LENGTH BY 1"
 
-    return length;
+         return length;
     }    
 
         
